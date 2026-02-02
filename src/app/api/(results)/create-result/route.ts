fac3773aa11f161f11fdccbb51d005f56fbbb596
LModipane <modipanesh@gmail.com>
2026-02-02 17:18:17 +0200

    test: console logged authheader

--- src/app/api/(results)/create-result/route.ts
+++ src/app/api/(results)/create-result/route.ts
@@ -1,11 +1,10 @@
 export async function POST(req: Request) {
-    try {
-        const authHeader = req.headers.get('authorization');
-        console.log("Auth Heeader:",authHeader)
-        console.log("Hello From Create Result Server Route!!!")
-        return new Response("Success", {status: 201})
-    } catch (error) {
-        console.error("Failled to create Match Result: ", error)
-        return new Response("Opps, failed to Post match Result", {status: 500})
-    }
+	try {
+		const authHeader = req.headers.get('authorization');
+		console.log('Hello From Create Result Server Route!!!', authHeader);
+		return new Response('Success', { status: 201 });
+	} catch (error) {
+		console.error('Failled to create Match Result: ', error);
+		return new Response('Opps, failed to Post match Result', { status: 500 });
+	}
 }

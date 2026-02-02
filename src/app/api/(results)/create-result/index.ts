353f5b7125ce9ab3ffaef10e16ca7e043c8956cf
LModipane <modipanesh@gmail.com>
2026-02-02 16:14:42 +0200

    fix: add successful response for create result cron-job

--- src/app/api/(results)/create-result/index.ts
+++ src/app/api/(results)/create-result/index.ts
@@ -1,8 +1,9 @@
 export async function POST() {
     try {
         console.log("Hello From Create Result Server Route!!!")
+        return new Response("Success", {status: 201})
     } catch (error) {
         console.error("Failled to create Match Result: ", error)
-        return new Response("Opps, failed to Post match Result")
+        return new Response("Opps, failed to Post match Result", {status: 500})
     }
 }

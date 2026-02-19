cf6df7dc29eb73f46f3f3cad29205db878190fa5
LModipane <modipanesh@gmail.com>
2026-02-19 18:00:09 +0200

    feat: implement ResultTable component and update LeaderPlace and FollowingPlace to use it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,12 +17,24 @@ import {
 	AccordionTrigger,
 	AccordionContent,
 } from '@/components/ui/accordion';
+
 import {
 	PredictionCard,
 	FixturesCarousel,
 	PredictionContextProvider,
 	CreatePredictionModelButton,
 } from '@/components';
+
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '@/components/ui/table';
+
+export const revalidate = 0;
 
 export default async function Home() {
 	const session = await getServerSession(authOptions);
@@ -131,7 +143,7 @@ const LeaderPlace = async ({ player }: LeaderPlaceProps) => {
 	return (
 		<Accordion type="single" collapsible className="w-full">
 			<AccordionItem value="Leader Place" className="w-full">
-				<AccordionTrigger className="w-full h-17.5 flex justify-between items-center bg-blue-800 text-left text-white font-bold text-lg">
+				<AccordionTrigger className="w-full h-17.5 flex justify-between items-center bg-blue-700 text-left text-white font-bold text-lg">
 					<span className="bg-pink-700 w-8 h-8 z-40 absolute -left-5 flex justify-center items-center">
 						1
 					</span>
@@ -145,93 +157,20 @@ const LeaderPlace = async ({ player }: LeaderPlaceProps) => {
 									className="object-cover"
 								/>
 							</div>
-							<h2 className="my-auto text-start text-xs w-33.75 truncate">{player.name}</h2>
+							<h2 className="my-auto text-start text-xl flex-1 truncate">{player.name}</h2>
 						</div>
-						<div className="border-r-2 border-white px-1 text-2xl mr-4">{player.score}</div>
+						<div className="border-r-2 border-white px-1 text-4xl mr-4">{player.score}</div>
 					</div>
 				</AccordionTrigger>
-				<AccordionContent>
-					<div className="bg-blue-800 dark:bg-blue-950 p-4 w-[90%] mx-auto">
-						{player.results && player.results.length !== 0 ? (
-							player.results.map((result, index) => (
-								<ResultCard key={result.id} result={result} index={index} />
-							))
-						) : (
-							<div className="text-center text-white">No results yet.</div>
-						)}
-					</div>
+				<AccordionContent className="w-full max-w-[90%] mx-auto text-white">
+					{player.results && player.results.length !== 0 ? (
+						<ResultTable results={player.results} />
+					) : (
+						<div className="text-center text-white">No results yet.</div>
+					)}
 				</AccordionContent>
 			</AccordionItem>
 		</Accordion>
-	);
-};
-
-type ResultCardProps = {
-	result: MatchResult;
-	index: number;
-};
-
-const ResultCard = ({ result, index }: ResultCardProps) => {
-	const fouls = 0;
-	return (
-		<div className="h-10 border-b-2 border-slate-400 w-full flex items-center">
-			<div className="border-x-2 border-slate-400 w-[10.5%] h-10 flex justify-center items-center">
-				<span>{++index}</span>
-			</div>
-			<div className="border-r-2 border-slate-400 w-[17.25%] h-10 flex justify-between items-center px-1">
-				<div className="relative min-w-4 min-h-4">
-					<Image src={result.homeTeamBadgeUrl} alt="home-team-logo" fill className="object-fit" />
-				</div>
-				<div className="flex justify-center items-center">
-					<EllipsisVertical className="h-3 w-3" />
-				</div>
-				<div className="relative min-w-4 min-h-4">
-					<Image src={result.awayTeamBadgeUrl} alt="home-team-logo" fill className="object-fit" />
-				</div>
-			</div>
-			<div className="border-r-2 border-slate-400 w-[17.25%] h-10 flex justify-center items-center">
-				<span className="text-lg">{result.homeTeamScoreResult}</span>
-				<div className="flex justify-center items-center">
-					<EllipsisVertical className="h-3 w-3" />
-				</div>
-				<span className="text-lg">{result.awayTeamScoreResult}</span>
-			</div>
-			<div className="border-r-2 border-slate-400 w-[18.5%] h-10 flex justify-center items-center">
-				<span className="text-lg">{result.homeTeamScorePrediction}</span>
-				<div className="flex justify-center items-center">
-					<EllipsisVertical className="h-3 w-3" />
-				</div>
-				<span className="text-lg">{result.awayTeamScorePrediction}</span>
-			</div>
-			<div
-				className={cn(
-					'border-r-2 border-slate-400 w-[17%] h-10 flex justify-center items-center',
-					fouls < 0 ? 'text-red-500' : 'text-gray-500',
-				)}>
-				{fouls > 0 ? (
-					<span>NA</span>
-				) : fouls < 0 ? (
-					<Minus className="h-4 w-4" />
-				) : (
-					<Diff className="h-4 w-4" />
-				)}
-				{fouls <= 0 ? <span className="text-lg">{Math.abs(fouls)}</span> : <></>}
-			</div>
-			<div
-				className={cn(
-					'border-r-2 border-slate-400 w-14 h-10 flex justify-center items-center ',
-					result.point! > 0
-						? 'text-green-500'
-						: result.point! < 0
-							? 'text-red-500'
-							: 'text-gray-500',
-				)}>
-				{result.point! > 0 ? <Plus className="h-4 w-4" /> : <></>}
-				{result.point! < 0 ? <Minus className="h-4 w-4" /> : <></>}
-				{result.point === 0 ? <Diff className="h-4 w-4" /> : <></>}
-				<span className="text-lg">{Math.abs(result.point!)}</span>
-			</div>
-		</div>
 	);
 };
 
@@ -244,7 +183,7 @@ const FollowingPlace = async ({ player, index }: FollowingPlaceProps) => {
 	return (
 		<Accordion type="single" collapsible className="w-[90%] mx-auto">
 			<AccordionItem value="Following Place" className="w-full">
-				<AccordionTrigger className="relative bg-blue-900 justify-center items-center">
+				<AccordionTrigger className="relative bg-blue-800 justify-center items-center">
 					<div className="absolute top-0 left-0 flex items-center justify-center w-5 h-5 p-2 bg-pink-700">
 						<span className="text-xs">{++index}</span>
 					</div>
@@ -257,14 +196,122 @@ const FollowingPlace = async ({ player, index }: FollowingPlaceProps) => {
 						/>
 					</div>
 					<div className="flex items-center justify-between w-full p-2">
-						<h3 className="text-xs text-start w-32 truncate">{player.name}</h3>
-						<span className="border-r-2 border-white pr-2 text-lg">{player.score}</span>
+						<h3 className="text-lg text-start flex-1 truncate">{player.name}</h3>
+						<span className="border-r-2 border-white pr-2 text-3xl">{player.score}</span>
 					</div>
 				</AccordionTrigger>
-				<AccordionContent>
-					<div className="bg-blue-800 dark:bg-blue-950 p-4 w-[90%] mx-auto">helloworld</div>
+				<AccordionContent className="w-full max-w-[95%] mx-auto text-white">
+					{player.results && player.results.length !== 0 ? (
+						<ResultTable results={player.results} />
+					) : (
+						<div className="text-center text-white">No results yet.</div>
+					)}
 				</AccordionContent>
 			</AccordionItem>
 		</Accordion>
+	);
+};
+
+type ResultTableProps = {
+	results: MatchResult[];
+};
+
+const ResultTable = ({ results }: ResultTableProps) => {
+	return (
+		<Table className="bg-blue-900 p-4 text-white ">
+			<TableHeader>
+				<TableRow className="flex items-center justify-start h-10 hover:bg-blue-800 border-b-2 border-slate-400">
+					<TableHead className="border-[1.5px] border-slate-500 w-14 h-10 flex justify-center items-center text-white">
+						#Pos
+					</TableHead>
+					<TableHead className="border-[1.5px] border-slate-500 flex-1 h-10 flex justify-center items-center text-white">
+						Match
+					</TableHead>
+					<TableHead className="border-[1.5px] border-slate-500 flex-1 h-10 flex justify-center items-center text-white">
+						Result
+					</TableHead>
+					<TableHead className="border-[1.5px] border-slate-500 flex-1 h-10 flex justify-center items-center text-white">
+						Prediction
+					</TableHead>
+					<TableHead className="border-[1.5px] border-slate-500 flex-1 h-10 flex justify-center items-center text-white">
+						Points
+					</TableHead>
+				</TableRow>
+			</TableHeader>
+			<TableBody>
+				{results.map((result, index) => (
+					<TableRow
+						key={result.id}
+						className="flex items-center justify-start h-10 hover:bg-blue-800 border-b-[1.5px] border-slate-400">
+						<TableCell className="border-[1.5px] border-slate-500 w-14 h-10 flex justify-center items-center">
+							<span>{++index}</span>
+						</TableCell>
+						<TableCell className="border-[1.5px] border-slate-500 flex-1 h-10 flex justify-center items-center gap-x-2">
+							<div className="relative min-w-6 min-h-6">
+								<Image
+									src={result.homeTeamBadgeUrl}
+									alt="home-team-logo"
+									fill
+									className="object-fit"
+								/>
+							</div>
+							<div className="flex justify-center items-center">
+								<EllipsisVertical className="h-3 w-3" />
+							</div>
+							<div className="relative min-w-6 min-h-6">
+								<Image
+									src={result.awayTeamBadgeUrl}
+									alt="home-team-logo"
+									fill
+									className="object-fit"
+								/>
+							</div>
+						</TableCell>
+						<TableCell className="border-[1.5px] border-slate-500 flex-1 h-10 flex justify-center items-center">
+							<span className="text-lg">{result.homeTeamScoreResult}</span>
+							<div className="flex justify-center items-center">
+								<EllipsisVertical className="h-3 w-3" />
+							</div>
+							<span className="text-lg">{result.awayTeamScoreResult}</span>
+						</TableCell>
+						<TableCell className="border-[1.5px] border-slate-500 flex-1 h-10 flex justify-center items-center">
+							<span className="text-lg">{result.homeTeamScorePrediction}</span>
+							<div className="flex justify-center items-center">
+								<EllipsisVertical className="h-3 w-3" />
+							</div>
+							<span className="text-lg">{result.awayTeamScorePrediction}</span>
+						</TableCell>
+						{/* <TableCell
+												className={cn(
+													'w-[17%] h-10 flex justify-center items-center',
+													fouls < 0 ? 'text-slate-500' : 'text-gray-500',
+												)}>
+												{fouls > 0 ? (
+													<span>NA</span>
+												) : fouls < 0 ? (
+													<Minus className="h-4 w-4" />
+												) : (
+													<Diff className="h-4 w-4" />
+												)}
+												{fouls <= 0 ? <span className="text-lg">{Math.abs(fouls)}</span> : <></>}
+											</TableCell> */}
+						<TableCell
+							className={cn(
+								'border-[1.5px] border-slate-500 flex-1 h-10 flex justify-center items-center',
+								result.point! > 0
+									? 'text-green-500'
+									: result.point! < 0
+										? 'text-red-500'
+										: 'text-gray-500',
+							)}>
+							{result.point! > 0 ? <Plus className="h-4 w-4" /> : <></>}
+							{result.point! < 0 ? <Minus className="h-4 w-4" /> : <></>}
+							{result.point === 0 ? <Diff className="h-4 w-4" /> : <></>}
+							<span className="text-lg">{Math.abs(result.point!)}</span>
+						</TableCell>
+					</TableRow>
+				))}
+			</TableBody>
+		</Table>
 	);
 };

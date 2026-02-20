fb903ba65880cfb3ef7ffbf27a1f4691f626d612
LModipane <modipanesh@gmail.com>
2026-02-20 09:02:08 +0200

    fix: improve layout and responsiveness in Home, LeaderTable, LeaderPlace, FollowingPlace, and ResultTable components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,8 +78,8 @@ export default async function Home() {
 		.orderBy(desc(totalScore));
 
 	return (
-		<main className="h-full w-full flex">
-			<section className="bg-purple-900 h-full max-w-[35%] p-2 text-white flex flex-col gap-4">
+		<main className="h-full w-full flex flex-col-reverse sm:flex-row overflow-scroll">
+			<section className="bg-purple-900 h-full sm:max-w-[35%] flex-1 p-2 text-white flex flex-col gap-4">
 				<PredictionContextProvider fixtures={fixtures}>
 					<FixturesCarousel fixtures={fixtures} />
 					<CreatePredictionModelButton fixtures={fixtures} />
@@ -107,7 +107,7 @@ type Props = {
 
 const LeaderTable = ({ leaderboard }: Props) => {
 	return (
-		<div className="relative flex flex-col w-full h-full p-4 px-10 ">
+		<div className="relative flex flex-col w-full h-full p-2 pl-5 sm:px-10">
 			<div className="absolute -right-3 z-10 mr-4 -top-2">
 				<h2 className="font-extrabold text-white text-[17px] uppercase stroke-colour">
 					Leaderboard
@@ -140,26 +140,26 @@ const LeaderPlace = async ({ player }: LeaderPlaceProps) => {
 	return (
 		<Accordion type="single" collapsible className="w-full">
 			<AccordionItem value="Leader Place" className="w-full">
-				<AccordionTrigger className="w-full h-17.5 flex justify-between items-center bg-blue-700 text-left text-white font-bold text-lg">
-					<span className="bg-pink-700 w-8 h-8 z-40 absolute -left-5 flex justify-center items-center">
+				<AccordionTrigger className="sm:w-full h-17.5 flex justify-between items-center bg-blue-700 text-left text-white font-bold">
+					<span className="bg-pink-700 w-8 h-8 z-40 absolute sm:-left-6 -left-4 flex justify-center items-center">
 						1
 					</span>
-					<div className="flex justify-between items-center w-full ">
-						<div className="flex justify-between item-center gap-x-2 mr-auto">
-							<div className="relative ml-3 min-h-16 min-w-16">
-								<Image
-									src={player.imageUrl ?? '/default-profile.png'}
-									alt={`${player.name} Image`}
-									fill
-									className="object-cover"
-								/>
-							</div>
-							<h2 className="my-auto text-start text-xl flex-1 truncate">{player.name}</h2>
+					<div className="flex justify-start items-center w-full">
+						<div className="absolute -top-2 left-0 sm:ml-3 ml-5 min-h-18 min-w-18">
+							<Image
+								src={player.imageUrl ?? '/default-profile.png'}
+								alt={`${player.name} Image`}
+								fill
+								className="object-cover"
+							/>
 						</div>
-						<div className="border-r-2 border-white px-1 text-4xl mr-4">{player.score}</div>
+						<h2 className="ml-25 mr-auto text-start sm:text-xl text-sm flex-1  truncate">
+							{player.name}
+						</h2>
+						<span className="border-r-2 border-white pr-2 text-4xl">{player.score}</span>
 					</div>
 				</AccordionTrigger>
-				<AccordionContent className="w-full max-w-[90%] mx-auto text-white">
+				<AccordionContent className="w-full sm:max-w-[90%] max-w-[98%] mx-auto text-white">
 					{player.results && player.results.length !== 0 ? (
 						<ResultTable results={player.results} />
 					) : (
@@ -178,26 +178,32 @@ type FollowingPlaceProps = {
 
 const FollowingPlace = async ({ player, index }: FollowingPlaceProps) => {
 	return (
-		<Accordion type="single" collapsible className="w-[95%] mx-auto">
+		<Accordion type="single" collapsible className="sm:w-[95%] w-[98%] mx-auto">
 			<AccordionItem value="Following Place" className="w-full">
-				<AccordionTrigger className="relative bg-blue-800 justify-center items-center">
-					<div className="absolute top-0 left-0 flex items-center justify-center w-5 h-5 p-2 bg-pink-700">
+				<AccordionTrigger className="relative bg-blue-800 justify-start items-center rounded-tl-md ">
+					<div
+						className={cn(
+							'absolute top-0 left-0 flex items-center justify-center w-5 h-5 p-2 bg-pink-700 ',
+							index === 1 ? 'rounded-tl-md' : '',
+						)}>
 						<span className="text-xs">{++index}</span>
 					</div>
-					<div className="relative ml-4 min-h-10 min-w-10">
-						<Image
-							src={player.imageUrl ?? '/default-profile.png'}
-							alt={`${player.name} Image`}
-							fill
-							className="border-2 rounded-full border-slate-600"
-						/>
-					</div>
-					<div className="flex items-center justify-between w-full p-2">
-						<h3 className="text-lg text-start flex-1 truncate">{player.name}</h3>
-						<span className="border-r-2 border-white pr-2 text-3xl">{player.score}</span>
+					<div className="flex justify-start items-center gap-x-2 w-full">
+						<div className="relative ml-7 min-h-10 min-w-10 ">
+							<Image
+								src={player.imageUrl ?? '/default-profile.png'}
+								alt={`${player.name} Image`}
+								fill
+								className="border-2 rounded-full border-slate-600"
+							/>
+						</div>
+						<div className="flex items-center justify-between w-full">
+							<h3 className="sm:text-lg text-sm text-start flex-1 truncate">{player.name}</h3>
+							<span className="border-r-2 border-white pr-2 text-3xl">{player.score}</span>
+						</div>
 					</div>
 				</AccordionTrigger>
-				<AccordionContent className="w-full max-w-[95%] mx-auto text-white">
+				<AccordionContent className="w-full sm:max-w-[95%]  mx-auto text-white">
 					{player.results && player.results.length !== 0 ? (
 						<ResultTable results={player.results} />
 					) : (
@@ -217,17 +223,17 @@ const ResultTable = ({ results }: ResultTableProps) => {
 	return (
 		<Table className="bg-blue-900 p-4 text-white ">
 			<TableHeader>
-				<TableRow className="flex items-center justify-start h-10 hover:bg-blue-800 border-b-2 border-slate-400">
-					<TableHead className="border-[1.5px] border-slate-500 w-14 h-10 flex justify-center items-center text-white">
-						#Pos
+				<TableRow className="flex items-center justify-end h-10 hover:bg-blue-800 border-b-2 border-slate-400">
+					<TableHead className="border-[1.5px] border-slate-500 sm:w-14 w-5 h-10 flex justify-center items-center text-white ">
+						# <span className="hidden sm:block">Pos</span>
 					</TableHead>
-					<TableHead className="border-[1.5px] border-slate-500 flex-1 h-10 flex justify-center items-center text-white">
+					<TableHead className="border-[1.5px] border-slate-500 flex-1 h-10 min-w-[70px] flex justify-center items-center text-white">
 						Match
 					</TableHead>
-					<TableHead className="border-[1.5px] border-slate-500 flex-1 h-10 flex justify-center items-center text-white">
+					<TableHead className="border-[1.5px] border-slate-500 flex-1 h-10 min-w-[40px] flex justify-center items-center text-white">
 						Result
 					</TableHead>
-					<TableHead className="border-[1.5px] border-slate-500 flex-1 h-10 flex justify-center items-center text-white">
+					<TableHead className="border-[1.5px] border-slate-500 flex-1 h-10 min-w-[70px] flex justify-center items-center text-white truncate">
 						Prediction
 					</TableHead>
 					<TableHead className="border-[1.5px] border-slate-500 flex-1 h-10 flex justify-center items-center text-white">
@@ -240,10 +246,10 @@ const ResultTable = ({ results }: ResultTableProps) => {
 					<TableRow
 						key={result.id}
 						className="flex items-center justify-start h-10 hover:bg-blue-800 border-b-[1.5px] border-slate-400">
-						<TableCell className="border-[1.5px] border-slate-500 w-14 h-10 flex justify-center items-center">
+						<TableCell className="border-[1.5px] border-slate-500 sm:w-14 w-5 h-10 flex justify-center items-center">
 							<span>{++index}</span>
 						</TableCell>
-						<TableCell className="border-[1.5px] border-slate-500 flex-1 h-10 flex justify-center items-center gap-x-2">
+						<TableCell className="border-[1.5px] border-slate-500 flex-1 h-10 w-[70px] flex justify-center items-center sm:gap-x-1">
 							<div className="relative min-w-6 min-h-6">
 								<Image
 									src={result.homeTeamBadgeUrl}
@@ -264,14 +270,14 @@ const ResultTable = ({ results }: ResultTableProps) => {
 								/>
 							</div>
 						</TableCell>
-						<TableCell className="border-[1.5px] border-slate-500 flex-1 h-10 flex justify-center items-center">
+						<TableCell className="border-[1.5px] border-slate-500 flex-1 h-10 min-w-[40px]  flex justify-center items-center">
 							<span className="text-lg">{result.homeTeamScoreResult}</span>
 							<div className="flex justify-center items-center">
 								<EllipsisVertical className="h-3 w-3" />
 							</div>
 							<span className="text-lg">{result.awayTeamScoreResult}</span>
 						</TableCell>
-						<TableCell className="border-[1.5px] border-slate-500 flex-1 h-10 flex justify-center items-center">
+						<TableCell className="border-[1.5px] border-slate-500 flex-1 h-10 min-w-[70px] flex justify-center items-center">
 							<span className="text-lg">{result.homeTeamScorePrediction}</span>
 							<div className="flex justify-center items-center">
 								<EllipsisVertical className="h-3 w-3" />

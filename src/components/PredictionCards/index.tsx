1013a2287675c1b71c1b67f835e33bd4b4f739db
LModipane <modipanesh@gmail.com>
2026-06-01 17:44:51 +0200

    fix: refactor PredictionCard component structure and styling for improved layout

--- src/components/PredictionCards/index.tsx
+++ src/components/PredictionCards/index.tsx
@@ -1,21 +1,22 @@
 'use client';
 
 import Image from 'next/image';
-import { EllipsisVertical, EyeOff, PenLine, Trash2 } from 'lucide-react';
+import { useModel } from '@/hooks';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { PredictionWithProfileMatchEvent } from '@/types';
 import usePredictionContext from '@/hooks/usePredictionContext';
+import { EllipsisVertical, EyeOff, PenLine, Trash2 } from 'lucide-react';
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+
 import {
 	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuGroup,
 	DropdownMenuItem,
 	DropdownMenuLabel,
-	DropdownMenuShortcut,
+	DropdownMenuGroup,
 	DropdownMenuTrigger,
+	DropdownMenuContent,
+	DropdownMenuShortcut,
 } from '../ui/dropdown-menu';
-import { useModel } from '@/hooks';
 
 type Props = {
 	groupId: string;
@@ -30,7 +31,7 @@ const PredictionCards = ({ groupId, predictions, currentProfileId }: Props) => {
 		: predictions;
 
 	return (
-		<ScrollArea className="flex flex-col gap-2 flex-1 pr-4 overflow-scroll no-scrollbar">
+		<ScrollArea className="flex flex-col gap-2 overflow-scroll no-scrollbar">
 			{filteredPredictions.map(prediction => (
 				<PredictionCard
 					key={prediction.id}
@@ -62,18 +63,18 @@ const PredictionCard = ({
 	const { onOpen } = useModel();
 
 	return (
-		<div className="flex items-center justify-between p-4 text-white">
+		<div className="flex items-center w-full max-w-full justify-between p-2 text-white ">
 			{/* Profile Section */}
-			<div className="flex items-center gap-3">
+			<div className="flex items-center gap-3 flex-1 min-w-7">
 				<Avatar className="size-9">
 					<AvatarImage src={profile.imageUrl || undefined} alt={profile.name || 'User Avatar'} />
 					<AvatarFallback>{profile.name ? profile.name[0] : 'U'}</AvatarFallback>
 				</Avatar>
-				<span className="font-medium text-xs truncate w-30">{profile.name}</span>
+				<span className="font-medium text-md w-full truncate">{profile.name}</span>
 			</div>
 
 			{/* Match Details Section */}
-			<div className="flex items-center gap-1">
+			<div className="flex items-center gap-1 ml-auto">
 				{/* Home Team Badge */}
 				<Image
 					src={matchEvent.homeTeamBadgeUrl}
@@ -84,7 +85,7 @@ const PredictionCard = ({
 				/>
 
 				{/* Prediction Section */}
-				<div className="flex items-center gap-2 font-semibold text-xl">
+				<div className="flex items-center ml-auto gap-2 font-semibold text-xl">
 					<span className="text-right">{homeTeamScore}</span>
 					<span>-</span>
 					<span className="text-left">{awayTeamScore}</span>
@@ -98,65 +99,65 @@ const PredictionCard = ({
 					height={32}
 					className="rounded-full"
 				/>
-
-				{/* Action Menu */}
-				{currentProfileId === profile.id && (
-					<DropdownMenu>
-						<DropdownMenuTrigger className="ml-2 cursor-pointer hover:bg-purple-600 p-1 rounded">
-							<EllipsisVertical size={24} />
-						</DropdownMenuTrigger>
-						<DropdownMenuContent
-							align="start"
-							side="right"
-							className="bg-purple-950 text-white shadow-lg p-2 boder-1 border-gray-400 rounded-lg">
-							<DropdownMenuGroup>
-								<DropdownMenuLabel>Prediction Menu</DropdownMenuLabel>
-								<hr className="border-gray-400" />
-								<div
-									className="hover:bg-purple-600 ml-2 p-2 flex items-center gap-x-4 rounded cursor-pointer capitalize"
-									// Note: Add Edit Prediction model in root layout page
-									onClick={() =>
-										onOpen('Prediction', {
-											predictionMode: 'EDIT',
-											match: matchEvent,
-											groupId,
-											leagueTagId: matchEvent.leagueTagId,
-											prevPrediction: {
-												id,
-												homeTeamScore,
-												awayTeamScore,
-											},
-										})
-									}>
-									<PenLine size={17} />
-									edit prediction
-									<DropdownMenuShortcut className="text-gray-400">Ctrl+P</DropdownMenuShortcut>
-								</div>
-								<div className="hover:bg-purple-600 ml-2 p-2 flex items-center gap-x-4 rounded cursor-pointer capitalize">
-									<EyeOff size={17} />
-									hide prediction
-									<DropdownMenuShortcut className="text-gray-400">Ctrl+H</DropdownMenuShortcut>
-								</div>
-								<div
-									className="hover:bg-red-900 ml-2 p-2 flex items-center gap-x-4 rounded cursor-pointer capitalize"
-									onClick={() =>
-										onOpen('Prediction', {
-											predictionMode: 'DELETE',
-											match: matchEvent,
-											groupId,
-											leagueTagId: matchEvent.leagueTagId,
-											prevPrediction: { id, homeTeamScore, awayTeamScore },
-										})
-									}>
-									<Trash2 size={17} />
-									remove prediction
-									<DropdownMenuShortcut className="text-gray-400">Ctrl+D</DropdownMenuShortcut>
-								</div>
-							</DropdownMenuGroup>
-						</DropdownMenuContent>
-					</DropdownMenu>
-				)}
 			</div>
+
+			{/* Action Menu */}
+			{currentProfileId === profile.id && (
+				<DropdownMenu>
+					<DropdownMenuTrigger className="cursor-pointer hover:bg-purple-600 p-1 rounded">
+						<EllipsisVertical size={24} />
+					</DropdownMenuTrigger>
+					<DropdownMenuContent
+						align="start"
+						side="right"
+						className="bg-purple-950 text-white shadow-lg p-2 boder-1 border-gray-400 rounded-lg">
+						<DropdownMenuGroup>
+							<DropdownMenuLabel>Prediction Menu</DropdownMenuLabel>
+							<hr className="border-gray-400" />
+							<div
+								className="hover:bg-purple-600 ml-2 p-2 flex items-center gap-x-4 rounded cursor-pointer capitalize"
+								// Note: Add Edit Prediction model in root layout page
+								onClick={() =>
+									onOpen('Prediction', {
+										predictionMode: 'EDIT',
+										match: matchEvent,
+										groupId,
+										leagueTagId: matchEvent.leagueTagId,
+										prevPrediction: {
+											id,
+											homeTeamScore,
+											awayTeamScore,
+										},
+									})
+								}>
+								<PenLine size={17} />
+								edit prediction
+								<DropdownMenuShortcut className="text-gray-400">Ctrl+P</DropdownMenuShortcut>
+							</div>
+							<div className="hover:bg-purple-600 ml-2 p-2 flex items-center gap-x-4 rounded cursor-pointer capitalize">
+								<EyeOff size={17} />
+								hide prediction
+								<DropdownMenuShortcut className="text-gray-400">Ctrl+H</DropdownMenuShortcut>
+							</div>
+							<div
+								className="hover:bg-red-900 ml-2 p-2 flex items-center gap-x-4 rounded cursor-pointer capitalize"
+								onClick={() =>
+									onOpen('Prediction', {
+										predictionMode: 'DELETE',
+										match: matchEvent,
+										groupId,
+										leagueTagId: matchEvent.leagueTagId,
+										prevPrediction: { id, homeTeamScore, awayTeamScore },
+									})
+								}>
+								<Trash2 size={17} />
+								remove prediction
+								<DropdownMenuShortcut className="text-gray-400">Ctrl+D</DropdownMenuShortcut>
+							</div>
+						</DropdownMenuGroup>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			)}
 		</div>
 	);
 };

2e294552dec08d0e95b4f9780f05e151e9302efa
LModipane <modipanesh@gmail.com>
2026-06-04 20:52:14 +0200

    fix: update PredictionCard to display 'Hidden' label for non-current profile predictions

--- src/components/PredictionCards/index.tsx
+++ src/components/PredictionCards/index.tsx
@@ -97,7 +97,7 @@ const PredictionCard = ({ groupId, prediction, currentProfileId }: PredictionCar
 				/>
 
 				{/* Prediction Section */}
-				{prediction.hide && prediction.profile.id === currentProfileId ? (
+				{prediction.hide && prediction.profile.id !== currentProfileId ? (
 					<div className="flex items-center ml-auto gap-2 font-semibold text-xl animate-pulse text-yellow-400">
 						<span className="text-right">Hidden</span>
 					</div>
